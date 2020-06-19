474c5dcdc758cf819246277ca762b9b1b26f0e62
Thiago Santos <thiagop.santos182@gmail.com>
2020-06-19 05:33:44 -0400

    Adding functionalities for creating and removing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from "react";
 
 import "./styles.css";
+import { useState } from "react";
+import { useEffect } from "react";
+
+import api from './services/api';
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories')
+      .then(response => {
+        setRepositories(response.data);
+      });
+  }, []);
+
+  async function handleAddRepository() { 
+    const response = await api.post('repositories', { 
+      title: `New Repository ${Date.now()}`, 
+      url:  'http://minhaapi.com.br/repos', 
+      techs: ['Javascript', 'Node.js', 'ReactJS'] 
+    }); 
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    
+    if (response.status === 204) {
+       setRepositories(repositories.filter(repository => repository.id !== id));
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { repositories.map(repository => 
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>            
+          )
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
